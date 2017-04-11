9a05364c39239b623ee4c4fbf166bee43d5817eb
Sam Alsmadi <Sam.Alsmadi@gmail.com>
2017-04-10 21:02:53 -0400

    Add JoinPoint

--- src/main/java/com/intellimed/AopMain.java
+++ src/main/java/com/intellimed/AopMain.java
@@ -11,6 +11,7 @@ public class AopMain {
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
 		ShapeService shapeService = context.getBean("shapeService", ShapeService.class);
+		shapeService.getCircle().setName("Dummy circle name");
 
 		System.out.println(shapeService.getCircle().getName());
 		//System.out.println(shapeService.getTriangle().getName());
--- src/main/java/com/intellimed/aspect/LoggingAspect.java
+++ src/main/java/com/intellimed/aspect/LoggingAspect.java
@@ -1,9 +1,12 @@
 package com.intellimed.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.intellimed.model.Circle;
 
 @Aspect
 public class LoggingAspect {
@@ -13,14 +16,22 @@ public class LoggingAspect {
 
 	//@Before("allGetters()")
 	@Before("allGetters() && allCircleMethods()")
-	public void LoggingAdvice(){
-		System.out.println("Advice is run. Invoking get method");
+	public void LoggingAdvice(JoinPoint joinPoint){
+		//Circle circle = (Circle) joinPoint.getTarget();
+		System.out.println("Advice is run. Invoking the method " + joinPoint.toString() + " on the object " + joinPoint.getTarget());
 	}
 	
 	@Before("allGetters()")
 	public void secondAdvice(){
 		System.out.println("Second Advice is run");
 	}
+	
+	@Before("args(name)")
+	public void stringArgumentAdvice(String name){
+		System.out.println("Invoked a method that takes a String argument. The provided value was " + name);
+
+	}
+	
 	
 	//@Before("execution(public String com.intellimed.model.Circle.getName())")
 	//@Before("execution(* get*())")
